b3f067cb4899a18f18a77b842bbbff9c7ec62f81
heidi505 <xxwhite19@gmail.com>
2023-07-25 19:18:14 +0900

    접두사인지 확인 정답

--- src/main/java/Prefix.java
+++ src/main/java/Prefix.java
@@ -1,15 +1,7 @@
 public class Prefix {
     public int solution(String my_string, String is_prefix) {
         int answer = 0;
-        boolean prefix = false;
-        char[] myArr = new char[is_prefix.length()];
-        char[] prefixArr = new char[is_prefix.length()];
-        for (int i = 0; i < is_prefix.length(); i++) {
-            myArr[i] = my_string.charAt(i);
-        }
-        if (prefixArr == myArr) {
-            prefix = true;
-        }
+        boolean prefix = my_string.startsWith(is_prefix);
         answer = prefix? 1 : 0;
         return answer;
     }
